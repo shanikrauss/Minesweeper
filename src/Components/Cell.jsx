acdd044c23396c7eb4d8cf3031f8e9b430876ac5
YehonatanSaharof <142481110+YehonatanSaharof@users.noreply.github.com>
2023-08-29 14:10:26 +0300

    cell created

--- src/Components/Cell.jsx
+++ src/Components/Cell.jsx
@@ -1,11 +1,15 @@
-function Cell({cellOpen, cellValue, cellCoordinates, handleClick}){
+function Cell({cellOpen, cellValue, cellCoordinates, handleClick, handleRightClick}){
   function handleOnClick(){
     handleClick(cellOpen, cellValue, cellCoordinates);
   }
 
+  function handleOnRightClick(){
+    handleRightClick(cellCoordinates);
+  }
+
   return(
       <div>
-        <button className={cellOpen ? `open ${cellValue}` : "closed"} onClick={handleOnClick} />
+        <button className={cellOpen ? `open ${cellValue}` : "closed"} onClick={handleOnClick} onRightClick={handleOnRightClick}/>
       </div>
   )
 }
